2e285917725f1f4612aae521d28ff2f5c2bfe2fd
Samuel <samuelpl0888@gmail.com>
2023-02-03 18:11:14 -0400

    Async await recall

--- index.js
+++ index.js
@@ -1,27 +1,43 @@
-// .first - after 1s first red;
-// .second - after 3s second blue; 4s
-// .third - after 2s third green; 6s
-// IN SEQUENCE !!!!
+const users = [
+  { id: 1, name: "john" },
+  { id: 2, name: "susan" },
+  { id: 3, name: "anna" },
+];
 
-const addColor = (time, selector, color) => {
-  const element = document.querySelector(selector);
-  return new Promise((resolve, reject) => {
-    if (element) {
-      setTimeout(() => {
-        element.style.color = color;
-        resolve();
-      }, time);
-    } else {
-      reject(`There's no such element: ${selector}`);
-    }
+const articles = [
+  { userId: 1, articles: ["one", "two", "three"] },
+  { userId: 2, articles: ["four", "five"] },
+  { userId: 3, articles: ["six", "seven", "eight", "nine"] },
+];
+
+const getUser = (name) =>
+  new Promise((resolve, reject) => {
+    const user = users.find((user) => user.name === name);
+    // If user found, if not
+    user ? resolve(user) : reject(`No such user with name : ${name}`);
   });
+
+const getArticles = (userId) =>
+  new Promise((resolve, reject) => {
+    const userArticles = articles.find((user) => user.userId === userId);
+    // If user found, if not
+    userArticles ? resolve(userArticles.articles) : reject("Wrong ID");
+  });
+
+const getData = async () => {
+  try {
+    const user = await getUser("john");
+    const articles = await getArticles(user.id);
+    console.log(articles);
+  } catch (error) {
+    console.log(error);
+  }
 };
 
-const btn = document.querySelector(".btn");
+getData();
 
-btn.addEventListener("click", () => {
-  addColor(1000, ".first", "red")
-    .then(() => addColor(3000, ".second", "blue"))
-    .then(() => addColor(2000, ".third", "green"))
-    .catch((err) => console.log(err));
-});
+// Other way, using promises
+// getUser("john")
+//   .then((user) => getArticles(user.id))
+//   .then((articles) => console.log(articles))
+//   .catch((err) => console.log(err));
